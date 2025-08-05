74e2d0d8157066eab72439f98b7a343182bf9408
Letícia F Mendonça <leticiamendonca2110@gmail.com>
2025-08-05 16:25:11 -0300

    Add files via upload

---
+++ 1º Semestre/Média de Três/exercicio4.js
@@ -0,0 +1,10 @@
+// 4. Cálculo da média de três números: Solicite três números ao usuário e calcule a média aritmética deles.
+
+let n1 = Number(prompt("Digite o primeiro número: "));
+let n2 = Number(prompt("Digite o segundo número: "));
+let n3 = Number(prompt("Digite o terceiro número: "));
+
+media = (n1+n2+n3)/3;
+
+alert("Média: " + media);
+console.log("Média: " + media);
---
+++ 1º Semestre/Nome Completo/exercicio2.js
@@ -0,0 +1,7 @@
+let nome = String(prompt("Digite seu nome: "));
+let sobrenome = String(prompt("Digite seu sobrenome: "));
+
+completo = nome + " " + sobrenome;
+
+alert("Nome completo: " + completo);
+console.log(completo);
